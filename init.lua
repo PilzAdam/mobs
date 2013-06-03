b254ecd9fc4177e6672c7de701466173a6578e44
PilzAdam <pilzadam@minetest.net>
2013-06-03 19:08:56 +0200

    Update models and textures; add dirt monster model

--- init.lua
+++ init.lua
@@ -3,10 +3,11 @@ dofile(minetest.get_modpath("mobs").."/api.lua")
 mobs:register_mob("mobs:dirt_monster", {
 	type = "monster",
 	hp_max = 5,
-	collisionbox = {-0.4, -1, -0.4, 0.4, 0.9, 0.4},
-	visual = "upright_sprite",
-	visual_size = {x=1, y=2},
-	textures = {"mobs_dirt_monster.png", "mobs_dirt_monster_back.png"},
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_stone_monster.x",
+	textures = {"mobs_dirt_monster.png"},
+	visual_size = {x=3, y=2.6},
 	makes_footstep_sound = true,
 	view_range = 15,
 	walk_velocity = 1,
@@ -25,6 +26,18 @@ mobs:register_mob("mobs:dirt_monster", {
 	light_damage = 2,
 	on_rightclick = nil,
 	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 14,
+		walk_start = 15,
+		walk_end = 38,
+		run_start = 40,
+		run_end = 63,
+		punch_start = 40,
+		punch_end = 63,
+	}
 })
 mobs:register_spawn("mobs:dirt_monster", {"default:dirt_with_grass"}, 3, -1, 7000, 3, 31000)
 
